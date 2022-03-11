e3743d57e5e1cf2c9a844fde99edfc929e801d66
Faem0220 <0faem@gmail.com>
2022-03-11 12:35:52 -0300

    add hardhat dir

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x7dd6E6C03ac4eBde5b07218D3D60e81bAB8EDA6B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-faem0220.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
